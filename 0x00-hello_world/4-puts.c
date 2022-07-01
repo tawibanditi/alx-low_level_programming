1546ca73a287cfd560a70237e6fe66ef3bb7f500
Tawanda <tawibanditi@gmail.com>
2022-07-01 08:55:19 -0700

    C program that prints

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  * main-Entry point
  * Return:Always 0 (success)
  */
-int main(void)
+int main (void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
