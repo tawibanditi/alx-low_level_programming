ed202772cb6a9dd4204cfc31b66bef6990735efa
Tawanda <tawibanditi@gmail.com>
2022-07-06 11:53:26 -0700

    Prints alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,11 @@
 #include "main.h"
 /**
- *main-print_alphabet-function to print abc
+ *main:print_alphabet-function to print abc
  * Return: Always 0 (success)
  */
 void print_alphabet(void)
 {
 char c;
-
 
 for (c = 'a'; c <= 'z'; c++)
 {		        
