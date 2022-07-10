4a418f75f9671d19750decd753c92fef0b06062a
Tawanda <tawibanditi@gmail.com>
2022-07-10 14:51:56 -0700

    prints a square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,19 +6,20 @@
  */
 void print_square(int size)
 {
-	int x;
-	int y;
-	if (size <= 0)
-	_putchar('\n');
-	else
-	{
-	for (x = 0; x < size; x++)
-	{
-	for (y = 0; y < size; y++)
-	{
-		_putchar(35);
-	}
-	_putchar('\n');
-	}
+int x;
+int y;
+
+if (size <= 0)
+_putchar('\n');
+else
+{
+for (x = 0; x < size; x++)
+{
+for (y = 0; y < size; y++)
+{
+_putchar(35);
+}
+_putchar('\n');
+}
 }
 }
