fb8a86797b8cb6a4cb95fadb98a682d0b299b340
Tawanda <tawibanditi@gmail.com>
2022-07-10 12:39:47 -0700

    Checks uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #incliude "main.h"
 
 /**
- * _isupper - this is a function to check uppercase alphas
+ * _isupper: this is a function to check uppercase alphas
  * @c: an integer
  * Return: Something but success
  */
