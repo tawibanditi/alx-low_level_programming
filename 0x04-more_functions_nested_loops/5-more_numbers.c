1240216ea3c15d57373c302cb0d1cd8a5a654bd5
Tawanda <tawibanditi@gmail.com>
2022-07-10 14:02:12 -0700

    print more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,15 +5,16 @@
  */
 void more_numbers(void)
 {
-	int x, y;
+	int x;
+	int y;
 
 	for (x = 0; x < 10; x++)
 	{
-	for (y = 0; y < 15; y++)
+		for (y = 0; y < 15; y++)
 	{
-	if (y >= 10)
-	_putchar((y / 10) + '0');
-	_putchar((y % 10) + '0');
+		if (y >= 10)
+		_putchar((y / 10) + '0');
+		_putchar((y % 10) + '0');
 
 	}
 _putcha('\n');
