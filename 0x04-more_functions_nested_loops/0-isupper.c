635cd7e82827907333f30e7168ba2252bb8f26ab
Tawanda <tawibanditi@gmail.com>
2022-07-10 12:01:52 -0700

    checking uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#incliude "main.h"
+
+/**
+ * _isuper - this is a function to check uppercase alphas
+ * @c: an integer
+ * Return: Something but success
+ */
+
+int _isupper(int c)
+{
+	return ( c >= 'A' && c <= 'Z');
+}
