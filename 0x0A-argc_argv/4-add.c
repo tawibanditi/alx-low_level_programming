5a2f2d5849ac259ebb95c246adf34bed2054ada0
Tawanda <tawibanditi@gmail.com>
2022-07-22 18:50:35 -0700

    adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
 {
 	int result = 0, num, i, j, k);
 
-	for (i = 0; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 			}
 		}
 	}
-	for (k = 0; k < argc; k++)
+	for (k = 1; k < argc; k++)
 	{
 		num = atoi(argv[k]);
 		result += num;
